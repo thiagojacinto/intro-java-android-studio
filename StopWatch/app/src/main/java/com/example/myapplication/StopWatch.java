c3de1a3528f1e7ac5fb5223b6f256cef4393bb16
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-02-20 00:09:57 -0300

    [WIP] trying to save elapsed time into an array

--- StopWatch/app/src/main/java/com/example/myapplication/StopWatch.java
+++ StopWatch/app/src/main/java/com/example/myapplication/StopWatch.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,12 +14,15 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+
 public class StopWatch extends AppCompatActivity {
 
     Button buttonStart, buttonStop;
     ImageView imageViewAnchor;
     Animation animationRounding;
     Chronometer mainTimer;
+    Long[] savedTime = new Long[5];
 
     private View.OnClickListener buttonStartClick = new View.OnClickListener() {
         @Override
@@ -33,9 +37,13 @@ public class StopWatch extends AppCompatActivity {
     private View.OnClickListener buttonStopClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            // saves the last time:
+            Long lastTimestamp = mainTimer.getBase();
             stopsChronometer();
             toggleStartButton();
             stopsAnchor();
+            // Stores time into an array:
+            savesTime(lastTimestamp);
         }
     };
 
@@ -116,5 +124,23 @@ public class StopWatch extends AppCompatActivity {
     private void stopsAnchor() {
 //        animationRounding.cancel();
         imageViewAnchor.setAnimation(null);
+    }
+
+    private void savesTime(Long timeToBeSaved) {
+        // OBJECTIVE: Stores time into an array
+        int topFive = savedTime.length; // this is always FIVE
+        if (topFive < 4) {
+            // when length is lower than limit
+            savedTime[topFive] = timeToBeSaved;
+        } else {
+            // pop the first element, and change the others
+            for (int i = 0; i < topFive - 2; i++) {
+                savedTime[i] = savedTime[i+1];
+            }
+            savedTime[topFive - 1] = timeToBeSaved;
+        }
+
+        Log.d("-> savesTime() ->", "Times: " + Arrays.toString(savedTime));
+
     }
 }
