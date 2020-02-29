bc351b0f782da72954d5e3801a8bd847d9277279
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-02-28 21:36:36 -0300

    [improv] DAO & SQL methods to make it work

--- DatabaseBabySteps/app/src/main/java/com/example/databasebabysteps/components/AddNote.java
+++ DatabaseBabySteps/app/src/main/java/com/example/databasebabysteps/components/AddNote.java
@@ -6,13 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.databasebabysteps.MainActivity;
 import com.example.databasebabysteps.R;
+import com.example.databasebabysteps.dao.NotesDAO;
+
+import java.sql.SQLException;
 
 public class AddNote extends AppCompatActivity {
 
     Button sendButton, backButton;
+    private NotesDAO notesDAO;
+    EditText noteImputed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +28,52 @@ public class AddNote extends AppCompatActivity {
         // Get elements
         backButton = findViewById(R.id.back_button);
         sendButton = findViewById(R.id.send_button);
+        noteImputed = findViewById(R.id.edit_text_input);
 
-        // On click BACK
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent backView = new Intent(AddNote.this, MainActivity.class);
-                startActivity(backView);
-            }
-        });
+        // Instantiate DAO
+        notesDAO = new NotesDAO(this);
+        try {
+            //
+            notesDAO.open();
 
-        // Save data = send to database dealing
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            // On click BACK
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent backView = new Intent(AddNote.this, MainActivity.class);
+                    startActivity(backView);
+                }
+            });
 
-            }
-        });
+            // Save data = send to database dealing
+            sendButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String noteToSave = noteImputed.getEditableText().toString();
+                    notesDAO.create(noteToSave);
+                    finish();
+                }
+            });
 
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
     }
 
+    @Override
+    protected void onResume() {
 
+        try {
+            notesDAO.open();
+            super.onResume();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        notesDAO.close();
+        super.onPause();
+    }
 }
