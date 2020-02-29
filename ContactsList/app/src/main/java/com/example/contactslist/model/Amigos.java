24d10369701e64567c56de730691f4040a1b34ee
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-02-29 10:45:04 -0300

    [model] Contact java class

---
+++ ContactsList/app/src/main/java/com/example/contactslist/model/Amigos.java
@@ -0,0 +1,37 @@
+package com.example.contactslist.model;
+
+public class Amigos {
+
+    int id;
+    String nome, local, dataencontro;
+    double numero;
+
+    public Amigos(int id, String nome, String local, String dataencontro, double numero) {
+        this.id = id;
+        this.nome = nome;
+        this.local = local;
+        this.dataencontro = dataencontro;
+        this.numero = numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public String getDataEncontro() {
+        return dataencontro;
+    }
+
+    public double getNumero() {
+        return numero;
+    }
+
+}
